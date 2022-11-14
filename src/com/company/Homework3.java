3cd6c7e5ef0649455aa29ced98c88a76a9332bea
Ichornomaz <nafertest@gmail.com>
2022-11-14 18:35:02 +0200

    2 homework all required tasks + 1 optional

---
+++ src/com/company/Homework3.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Homework3 {
+//    7.	Найти среднее арифметическое элементов массива.
+    public static void avgArr(){
+        int[] numbers = new int[20];
+
+        // Создаем массив случайных чисел от 0 до 99
+        for(int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(10);
+            System.out.print(numbers[i] + " ");
+        }
+    }
+}
