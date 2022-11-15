fe8cce879daf7f094668e0ac8b9587d9fedb835d
Ichornomaz <nafertest@gmail.com>
2022-11-15 21:13:55 +0200

    homework 3 6task

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,11 +8,11 @@ public class Main {
 //        lesson1.getNearestNumber();
 //        lesson1.workWithDollarsEuroPounds();
 //
-//        Lesson2_Arrays_Loops lesson2 = new Lesson2_Arrays_Loops();
+        Lesson2_Arrays_Loops lesson2 = new Lesson2_Arrays_Loops();
 //        lesson2.createArrayOfArrays();
 //        lesson2.getMaxAndMinFromArray();
 //        lesson2.calculator();
-//        lesson2.exceptionsCatching();
+        lesson2.exceptionsCatching();
 //
 //        Lesson3_Arrays_Loops lesson3 = new Lesson3_Arrays_Loops();
 //        lesson3.getAverageFromArray();
